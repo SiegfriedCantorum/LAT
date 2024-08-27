972515955e36d287b0540faa17d41a4453ed77a5
Siegfried Hadatsch <s.hadatsch@laborvolkmann.de>
2024-08-27 12:40:28 +0200

    changed lat.pl version origin

--- bin/lat.pl
+++ bin/lat.pl
@@ -3,10 +3,10 @@
 # ---------------------------------------------------------
 # lat.pl                           
 # Autor: shad
-# Version: 1.3 'facelift' (2024-06-19 build)
+# Version: 1.2.2 'autoswitch-fixes' (2024-06-27 build)
 # Neuerungen:
-#       
-#   - Allgemeine Ueberarbeitung des Codes: Vereinfachung, streamlining, De-Spaghettifizierung 
+#       - "-c" prueft nun auch die laenge von Datumsfeldern und gibt eine fehlermeldung aus, wenn die laenge nicht regelkonform ist
+#       - Anpassung der Pfade der ldt-Referenztabellen im Programm
 #
 # --------------------------------------------------------- 
 # Kommandozeilentool zur graphisch aufbereiteten Anzeige von .ldt-Dateien 
@@ -14,8 +14,9 @@
 # "lat" liest die Eingabedatei(en) Zeilenweise aus, bereitet Sie entsprechend der Eingabeparameter auf und gibt das Ergebniss nach STDOUT aus.
 #        
 #       - Standardeinstellungen:
+#
 #           - Ausgabe: FELDZIFFER - FELDINHALT (Rest jeder Zeile wird entfernt)
-#           - LDT-Standard: LDT 2 gemaess KBV-Feldtabelle (Referenztabellen fuer Bedeutungen von Feldziffern liegen auf der laborsrv unter /laborsrv/parameterdateien/ldt_feldtabellen/)
+#           - LDT-Standard: LDT 2 gemaess KBV-Feldtabelle (Referenztabellen fuer Bedeutungen von Feldziffern liegen auf der laborsrv unter /laborsrv/parameterdateien/LDT/ldt/)
 #           - LAT blendet Objektidentifier (nur in LDT3 vorhanden) automatisch aus
 #       
 # ---------------------------------------------------------
@@ -26,19 +27,19 @@
 # ---------------------------------------------------------
 #   GEPLANTE UPDATES:                                                               
 #
-#       - 1.X ('Troyes')       - Anpassung des Check-Moduls an LDT3 Standards
+#       - 1.3 ('checker'):     - Implementierung relevanter Inhaltschecks fuer LDT3-Dateien
+#                              - Erkennung des Pruefungsmusters basierend auf erkannter Version (1.2 Update)
 #                 
-#       - 1.X ('springclean')  - Ueberarbeitung der Verarbeitungsablaeufe des Tools: Streamlining, Entfernen von redunanten loops, Vereinheitlichungen von Variablen, Funktionen etc
+#       - 1.4 ('springclean')  - Ueberarbeitung der Verarbeitungsablaeufe des Tools: Streamlining, Entfernen von redunanten loops, Vereinheitlichungen von Variablen, Funktionen etc
 #                              - Modularisierung / Packaging erforderlicher libraries -> nach Moeglichkeit keine statische Adresse der Referenztabellen auf Server, sondern in tool hinterlegt
 #
-#       - 1.X ('tbd')          - Neuer Parser ?
+#       - 1.5 ('tbd')          - Neuer Parser ?
 #                              - Allgemeine Vereinfachungen ? 
 #                              - Updates der Referenztabellen ?
 #        
 ######################################################################################
 
 use strict;
-use Config::Tiny;
 use Getopt::Mixed 1.006, 'getOptions'; #Aktuellere Module wie Getopt::Long liegen nicht auf der linuxsrv!
 use File::Find;
 use vars qw($opt_bytecount $opt_feld $opt_objektid $opt_verbose $opt_switch $opt_check $opt_help $opt_version $opt_dynamic);
@@ -67,7 +68,7 @@ sub hilfe { #Ausgabe Hilfstext bei -h /--help
 }
 
 sub version{#Ausgabe Version --version
-    print "\nLDT-AnalyseTool Version 1.3 'facelift' (2024-06-19 build)\n\n";
+    print "\nLDT-AnalyseTool Version 1.2.2 'autoswitch' (2024-06-27 build)\n\n";
     print "Tool zur grafisch aufbereiteten Ausgabe und Analyse von Dateien des LDT-Standards \n";
     print "Geschrieben von Siegfried Hadatsch (-shad)\n\nAnregungen, Anmerkungen und Bugs an s.hadatsch(at)volkmann.de - Merci! :D \n";
     print "\n";  
@@ -81,7 +82,15 @@ foreach my $input_datei (@ARGV) { #Erstelle Liste aller eingegebenen Dateien
     push @arbeitsdateien, $input_datei;
 }
 
-if($opt_verbose){$opt_bytecount = 1, $opt_feld = 1, $opt_objektid = 1, $opt_verbose = 1}  #Setze verbose-modus auf aktiv
+sub lade_referenztabelle_ldt {  #Subroutine zum global laden von LDT-Versions-Referenztabelle (Standard LDT2)
+    #my $ldt_tabelle = $opt_switch ? './lib/ldt3.pl': './lib/ldt2.pl'; #Auf linuxsrv auskommentieren!!!
+    my $ldt_tabelle = $opt_switch ? '/laborsrv/parameterdateien/ldt_feldtabellen/ldt3.pl': '/laborsrv/parameterdateien/ldt_feldtabellen/ldt2.pl'; #In IDE auskommentieren!! #PFADE STATISCH FUER linuxsrv
+    unless (defined $ldt_tabelle){die "Feldtabelle nicht gefunden!"}
+    my $ldt_tabelle_geladen = do $ldt_tabelle;
+    return $ldt_tabelle_geladen;
+}
+
+if($opt_verbose){$opt_bytecount = 1, $opt_feld = 1, $opt_check = 1, $opt_objektid = 1, $opt_verbose = 1}  #Setze verbose-modus auf aktiv
 
 foreach my $input_datei (@arbeitsdateien){ #Hier beginnt der loop fuer die Batch-Verarbeitung
 
@@ -91,32 +100,31 @@ close $datei;
 
 #### Bestimmung Version des Datensatzes #####
 
-my $ver_satzbeschreibung = "Unbekannt! (Wechsel zu default: LDT2)";
-my $ldt_tabelle_geladen;
-my $ldt_tabelle; 
-                    
+my $ver_satzbeschreibung = $opt_switch ? "NICHT ERKANNT - Weiter mit Standard LDT3" : "NICHT ERKANNT - Weiter mit Standard LDT2"; #Platzhalter falls keine Version erkannt wurde + Fall $opt_switch
+my $ldt_tabelle_geladen = {}; #Referenztabelle initialisieren
+my $version_found = 0; #flag, ob Versionsfeldkennung gefunden wurde
+
 for my $z (@zeilen){ #Wir bestimmen die Version des LDT-Datensatzes basierend auf der Feldkennung, die die Datei verwendet    
-    my $feldkennung = substr($z, 3, 4);
-    
-    if ($feldkennung == 9212){ #LDT1 + LDT2 : Version in Feldkennung 9212 
-        $ldt_tabelle = '/c/Users/hadatsch/Desktop/Projects/enw/lat/Releases/00_lib/lib/ldt2.pl'; #Auf linuxsrv auskommentieren!!!
-        #$ldt_tabelle = '/laborsrv/parameterdateien/ldt_feldtabellen/ldt2.pl'; #In IDE auskommentieren!! #PFADE STATISCH FUER linuxsrv
-        $ver_satzbeschreibung = substr($z,7);
-    } elsif ($feldkennung == 0001){ #LDT3 : Version in Feldkennung 0001 hinterlegt
-        $ldt_tabelle = '/c/Users/hadatsch/Desktop/Projects/enw/lat/Releases/00_lib/lib/ldt3.pl';#Auf linuxsrv auskommentieren!!!
-        #my $ldt_tabelle = '/laborsrv/parameterdateien/ldt_feldtabellen/ldt3.pl'; #In IDE auskommentieren!! #PFADE STATISCH FUER linuxsrv
-        $ver_satzbeschreibung = substr($z,7);
+    if (substr($z, 3, 4) == 9212){ #LDT1 + LDT2 : Version in Feldkennung 9212 c
+
+            $ver_satzbeschreibung = substr($z, 7);
+            $version_found = 1;
+            #my $ldt_tabelle = './lib/ldt2.pl'; #Auf linuxsrv auskommentieren!!!
+            my $ldt_tabelle = '/laborsrv/parameterdateien/ldt_feldtabellen/ldt2.pl'; #In IDE auskommentieren!! #PFADE STATISCH FUER linuxsrv
+            $ldt_tabelle_geladen = do $ldt_tabelle;
+       }
+    elsif (substr($z, 3, 4) == 0001){ #LDT3 : Version in Feldkennung 0001 hinterlegt
+            $ver_satzbeschreibung = substr($z, 7);
+            $version_found = 1;
+            #my $ldt_tabelle = './lib/ldt3.pl';#Auf linuxsrv auskommentieren!!!
+            my $ldt_tabelle = '/laborsrv/parameterdateien/ldt_feldtabellen/ldt3.pl'; #In IDE auskommentieren!! #PFADE STATISCH FUER linuxsrv
+            $ldt_tabelle_geladen = do $ldt_tabelle;
+       }
     }
 
-    if (defined $ldt_tabelle){
-        $ldt_tabelle_geladen = do $ldt_tabelle;
-    } else {
-        $ldt_tabelle = '/c/Users/hadatsch/Desktop/Projects/enw/lat/Releases/00_lib/lib/ldt2.pl';
-        #my $ldt_tabelle = '/laborsrv/parameterdateien/ldt_feldtabellen/ldt2.pl'; #In IDE auskommentieren!! #PFADE STATISCH FUER linuxsrv
-        $ldt_tabelle_geladen = do $ldt_tabelle; #default-pfad
-        }
-    }
-
+if (!$opt_dynamic || !$version_found){ #dynamic regelt die automatische Erkennung der LDT-Version - falls nicht aktiviert ist oder keine Versions-Feldkennung gefunden: Standard LDT2
+    $ldt_tabelle_geladen = lade_referenztabelle_ldt}
+unless (defined $ldt_tabelle_geladen){die "Feldtabelle nicht gefunden!"}
 #### Bestimmung Datensatz Version Ende ####
 
 ############# Block zur regulaeren Ausgabe von Datei ##############
@@ -125,42 +133,46 @@ print"\n\n+--+--+--+--+--+--+--+--+--+--+--+--+-+--+--+--+--+--+--+--+--+--+--+-
 print "\nVerarbeitung: $input_datei (Version $ver_satzbeschreibung)\n\n\n";
 
 my $feldname = "";
-my $zn = "";                                  #Zeilencounter
+my $zn = ""; #Zeilencounter
 my @obj_idents = qw(8001 8002 8003);          #Feldnummern von Objektidents zum Ausblenden (LDT3)
 
     foreach my $zeile (@zeilen){ #Iteriere durch jede zeile in @zeilen und teile sie auf 
-        $zn ++;
-        my $zeilennummer = !$opt_verbose ? "" : "Z.$zn \t"; #definiere Zeilennummer
+        $zn +=1;
+        my $zeilennummer = "Z.$zn \t"; #definiere Zeilennummer
+        my $bytecount = substr($zeile, 0, 3) . "\t";          #Bytecount
+            if (!$opt_bytecount){$bytecount = ""}             #Flag f. Ausgabe Bytecount
+        my $feldkennung = substr($zeile, 3, 4) . "\t";        #Feldname
+        my $feldinhalt = substr($zeile, 7);                   #Feldinhalt
 
-        my $bytecount =  !$opt_bytecount ? "" : substr($zeile, 0, 3) . "\t";    #Bytecount + Flag Ausgabe
-        my $feldkennung = substr($zeile, 3, 4) . "\t";                          #Feldname
-        my $feldinhalt = substr($zeile, 7);                                     #Feldinhalt
         my $bytecount_int = $bytecount +0;      #setze zeileninhalt auf int
         my $feldkennung_int = $feldkennung +0;  #          --||--
 
-        if (!$opt_objektid && grep{ $_ eq $feldkennung_int} @obj_idents) {next;} #Skippe Objekt_IDs
+        if (!$opt_objektid){ # Flag Ein/Ausblendung Objektidents (LDT3)
+            if (grep{ $_ eq $feldkennung_int} @obj_idents) {next}} #skippe zeile, falls Feldkennung Objektident ist
         
-        my $feldname = "";
         if ($opt_feld){ #Flag zur Ausgabe von Feldnamen
             $feldname = $ldt_tabelle_geladen->{$feldkennung_int} || "------- Feldkennung unbekannt -------"; # lade feldname aus referenztabelle
             $feldname = sprintf("%-70s", $feldname)} #vereinheitliche Whitespaces in der ausgabe nach STDOUT
-
+            
+        if(!$opt_verbose){ #zeilennummer ist standardmaessig ausgeblendet
+            $zeilennummer = ""}
         my $ausgabe = "$zeilennummer$bytecount$feldkennung$feldname$feldinhalt"; 
         print "$ausgabe";
     }
 
-############## Anfang Block inhaltlicher Check von Datei ################
+############## Ende Block zur regulaeren Ausgabe von Datei ##############
 
+############## Anfang Block inhaltlicher Check von Datei ################
 if($opt_check){
     print"\n--------------------- Vollständigkeitspruefung ($input_datei) --------------------- \n\n"; #Wir bauen eine Kopfzeile
 
 #### hier beginnt der check-marathon #### TODO : Hier wird irgendwann mal auch der switch basierend auf Satzversion stehen!
 
-    my @adressen_fk = qw(3107 8321 8607); #Feldkennungen von Adressfeldern
+    my @adressen_fk = qw(3107); #Feldkennungen von Adressfeldern
     my @LANR_BSNR_fk = qw(0201 4217 4218 4241 4242 4248 4249); #Feldkennungen von LANR o. BSNR mit Laenge NEUN
     my @datumsfelder_fk = qw(0227 3103 3424 3425 3471 4102 4109 4110 4115 4133 4206 4214 4235 4247 4264 4265 4266 4268 4276 4277 4278 5000 5025 5026 5034 9103 9115 9116 9122); #Feldkennungen von Datumsfeldern mit Laenge ACHT
     my $zeilennummer = 0; #Counter fuer zeilennummer
-
+    #TO DO 4125 4233 mit 16!!!
     for my $zeile (@zeilen){# Pruefe 'einfache' Fehlerbedingungen wie Bytecount, invalid characters, falsche laenge von BSNR / LANR
         $zeilennummer +=1;
         $zeile =~ s/^\s+|\s+$//g; #Entferne Whitespace vor und nach Zeileninhalt
@@ -169,13 +181,12 @@ if($opt_check){
         my $feldinhalt = substr($zeile, 7);
         my $feldname = $ldt_tabelle_geladen->{int($feldkennung)} || "------- Feldkennung unbekannt -------";
 
-        #Lasst uns nach non-ints in Feldkennung und Bytecount suchen und Fehlermeldungen generieren
-        if ($bytecount =~ /\D/) {
-           push @fehlermeldungen, "$zeilennummer '$zeile': Bytecount ($bytecount) enthält ungueltige Zeichen.\n";
-            }
-        if ($feldkennung =~ /\D/) {         
-            push @fehlermeldungen, "$zeilennummer '$zeile': Feldkennung ($feldkennung) enthält ungueltige Zeichen.\n";
-            }              
+        if ($bytecount =~ /\D/ || $feldkennung =~ /\D/){ ### Ungueltige Zeichen in Bytecount/Feldkennung ####
+            if ($bytecount =~ /\D/) {
+               push @fehlermeldungen, "$zeilennummer '$zeile': Bytecount ($bytecount) enthält ungueltige Zeichen.\n";}
+            if ($feldkennung =~ /\D/) {         
+                push @fehlermeldungen, "$zeilennummer '$zeile': Feldkennung ($feldkennung) enthält ungueltige Zeichen.\n";}
+            }           
         if (grep {$_ eq $feldkennung} @adressen_fk) { #Pruefe ob Adressfelder Zahlen enthalten        
             if ($feldinhalt =~ /\d/){
                 push @fehlermeldungen, "$zeilennummer '$zeile': Adressefeld '$feldinhalt' enthält Zahlen.\n"}
@@ -207,7 +218,7 @@ if($opt_check){
         }
           
         my $bytecount_errechnet = length($feldinhalt)+9; # Berechne Bytecount gemäß KBV-Formel
-        if ($bytecount_errechnet != $bytecount && $bytecount !~ /\D/){ #Keine Berechnung, wenn Falsche Zeichen in Bytelänge vorhanden
+        if ($bytecount_errechnet != $bytecount && !($bytecount =~ /\D/)){ #Keine Berechnung, wenn Falsche Zeichen in Bytelänge vorhanden
             push @fehlermeldungen, "$zeilennummer '$zeile': Falsche Bytelänge ('$bytecount') angegeben (Errechnet: $bytecount_errechnet - Sonderzeichen können zu Problemen bei Berechnung fuehren.) \n";  
         }
     } #Hier endet der Check fuer "einfache" Fehler
